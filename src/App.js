28a90721cdd68a8543ef2a5401d797da84736a9d
thomas.lin <thomaslwq@gmail.com>
2020-06-29 09:45:08 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+/* 
+  1. 引入的sass 文件默认 后缀名 .scss
+*/
+import './App.scss';
+import Icon from "./Icon"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>标题</h1>
     </div>
   );
 }
